3c45398579e3314f343107a762509bc9bd623705
Muhammad Arslan <gery_maz_555@hotmail.com>
2023-08-23 10:03:28 +0500

    incomplete

--- app/src/main/java/com/mhdarslan/retrofitpractice/MainActivity.java
+++ app/src/main/java/com/mhdarslan/retrofitpractice/MainActivity.java
@@ -3,12 +3,20 @@ package com.mhdarslan.retrofitpractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    interface RequestUser{
+        @GET("/api/user/{uid }")
+        void getUser(@Path("uid")String uid);
+    }
+    TextView textview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textview = findViewById(R.id.textview);
     }
 }
